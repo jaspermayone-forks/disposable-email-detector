3b3411cd4398b7e79dd55005044834aa857241e8
Jasper Mayone <me@jaspermayone.com>
2025-03-07 03:22:14 +0000

    fix bug

--- index.ts
+++ index.ts
@@ -1,77 +1,93 @@
-import fs from 'fs/promises';
-import path from 'path'; 
+import fs from "fs/promises";
+import path from "path";
 
 let cachedDomains: string[] | undefined = undefined;
 
 const loadDomains = async () => {
-  if(cachedDomains) return cachedDomains;
-  const disposableDomainsBuffer = await fs.readFile(path.join(__dirname, 'index.json')); 
+  if (cachedDomains) return cachedDomains;
+  const disposableDomainsBuffer = await fs.readFile(
+    path.join(__dirname, "index.json")
+  );
   const disposableDomains = JSON.parse(disposableDomainsBuffer.toString());
   cachedDomains = disposableDomains;
   return disposableDomains;
-}
+};
 
 export class ExternalSourceError extends Error {
-  constructor(message: string = 'URL must be provided when loading from external source') {
+  constructor(
+    message: string = "URL must be provided when loading from external source"
+  ) {
     super(message);
-    this.name = 'ExternalSourceError';
+    this.name = "ExternalSourceError";
     Object.setPrototypeOf(this, ExternalSourceError.prototype);
   }
 }
 
-const loadDomainsFromExternalSource = async ({github, url}: LoadFromSource) => {
-    if (cachedDomains) return cachedDomains;
-    const URL = github ? "https://raw.githubusercontent.com/IntegerAlex/disposable-email-detector/refs/heads/main/index.json" : url;
-    if (!URL) throw new ExternalSourceError();
-    try {
-        const response = await fetch(URL);
-        const disposableDomains = await response.json();
-        cachedDomains = disposableDomains
-        return disposableDomains;
-    } catch (error) {
-        throw new ExternalSourceError("Failed to load disposable domains from the provided URL");
-    }
-}
+const loadDomainsFromExternalSource = async ({
+  github,
+  url,
+}: LoadFromSource) => {
+  if (cachedDomains) return cachedDomains;
+  const URL = github
+    ? "https://raw.githubusercontent.com/IntegerAlex/disposable-email-detector/refs/heads/main/index.json"
+    : url;
+  if (!URL) throw new ExternalSourceError();
+  try {
+    const response = await fetch(URL);
+    const disposableDomains = await response.json();
+    cachedDomains = disposableDomains;
+    return disposableDomains;
+  } catch (error) {
+    throw new ExternalSourceError(
+      "Failed to load disposable domains from the provided URL"
+    );
+  }
+};
 
 type LoadFromSource = {
-    github?: boolean;
-    url?: string;
-}
+  github?: boolean;
+  url?: string;
+};
 
 type Options = {
-    loadFromSource?: LoadFromSource;
-}
+  loadFromSource?: LoadFromSource;
+};
 
 // Function to detect disposable email addresses
-export default async function disposableEmailDetector(email: string, options?: Options): Promise<boolean> {
+export default async function disposableEmailDetector(
+  email: string,
+  options?: Options
+): Promise<boolean> {
   try {
-      let disposableDomains = [];
+    let disposableDomains: string[] = [];
     // Load from external source
     if (options?.loadFromSource) {
-        disposableDomains = await loadDomainsFromExternalSource(options.loadFromSource);
+      disposableDomains = await loadDomainsFromExternalSource(
+        options.loadFromSource
+      );
     } else {
-        disposableDomains = await loadDomains();
+      disposableDomains = await loadDomains();
     }
-    
-    
+
     // Extract the domain from the email address
-    const domain = email.split('@')[1].toLowerCase(); // Get the domain part of the email address and convert it to lowercase
+    const domain = email.split("@")[1].toLowerCase(); // Get the domain part of the email address and convert it to lowercase
 
-    // Check if the domain is in the list of disposable domains 
+    // Check if the domain is in the list of disposable domains
     return disposableDomains.includes(domain);
-
   } catch (error: any) {
-    if (error.code === 'ENOENT') {
-      console.error('index.json not found. Please create it with disposable domains.');
+    if (error.code === "ENOENT") {
+      console.error(
+        "index.json not found. Please create it with disposable domains."
+      );
     } else if (error instanceof SyntaxError) {
-      console.error('Invalid JSON format in index.json. Please correct the file.');
+      console.error(
+        "Invalid JSON format in index.json. Please correct the file."
+      );
     } else if (error instanceof ExternalSourceError) {
-        console.error(error.message);
+      console.error(error.message);
+    } else {
+      console.error("Unexpected error:", error);
     }
-    else {
-      console.error('Unexpected error:', error);
-    }
-    return false; 
+    return false;
   }
 }
-
