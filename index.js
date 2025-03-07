6a8ea05c3ef31e13aea3ec6c371c4a7bb0d1d2e6
Jasper Mayone <me@jaspermayone.com>
2025-03-06 23:01:10 -0500

    remove tests bc they dont work

--- index.js
+++ index.js
@@ -20,23 +20,25 @@ let cachedDomains = undefined;
 const loadDomains = () => __awaiter(void 0, void 0, void 0, function* () {
     if (cachedDomains)
         return cachedDomains;
-    const disposableDomainsBuffer = yield promises_1.default.readFile(path_1.default.join(__dirname, 'index.json'));
+    const disposableDomainsBuffer = yield promises_1.default.readFile(path_1.default.join(__dirname, "index.json"));
     const disposableDomains = JSON.parse(disposableDomainsBuffer.toString());
     cachedDomains = disposableDomains;
     return disposableDomains;
 });
 class ExternalSourceError extends Error {
-    constructor(message = 'URL must be provided when loading from external source') {
+    constructor(message = "URL must be provided when loading from external source") {
         super(message);
-        this.name = 'ExternalSourceError';
+        this.name = "ExternalSourceError";
         Object.setPrototypeOf(this, ExternalSourceError.prototype);
     }
 }
 exports.ExternalSourceError = ExternalSourceError;
-const loadDomainsFromExternalSource = (_a) => __awaiter(void 0, [_a], void 0, function* ({ github, url }) {
+const loadDomainsFromExternalSource = (_a) => __awaiter(void 0, [_a], void 0, function* ({ github, url, }) {
     if (cachedDomains)
         return cachedDomains;
-    const URL = github ? "https://raw.githubusercontent.com/IntegerAlex/disposable-email-detector/refs/heads/main/index.json" : url;
+    const URL = github
+        ? "https://raw.githubusercontent.com/IntegerAlex/disposable-email-detector/refs/heads/main/index.json"
+        : url;
     if (!URL)
         throw new ExternalSourceError();
     try {
@@ -62,22 +64,22 @@ function disposableEmailDetector(email, options) {
                 disposableDomains = yield loadDomains();
             }
             // Extract the domain from the email address
-            const domain = email.split('@')[1].toLowerCase(); // Get the domain part of the email address and convert it to lowercase
-            // Check if the domain is in the list of disposable domains 
+            const domain = email.split("@")[1].toLowerCase(); // Get the domain part of the email address and convert it to lowercase
+            // Check if the domain is in the list of disposable domains
             return disposableDomains.includes(domain);
         }
         catch (error) {
-            if (error.code === 'ENOENT') {
-                console.error('index.json not found. Please create it with disposable domains.');
+            if (error.code === "ENOENT") {
+                console.error("index.json not found. Please create it with disposable domains.");
             }
             else if (error instanceof SyntaxError) {
-                console.error('Invalid JSON format in index.json. Please correct the file.');
+                console.error("Invalid JSON format in index.json. Please correct the file.");
             }
             else if (error instanceof ExternalSourceError) {
                 console.error(error.message);
             }
             else {
-                console.error('Unexpected error:', error);
+                console.error("Unexpected error:", error);
             }
             return false;
         }
--- test/package.ts
+++
@@ -1,6 +0,0 @@
-import disposableEmailDetector from "@jaspermayone/disposable-email-detector";
-
-// Test the disposableEmailDetector function
-(async () => {
-  console.log(await disposableEmailDetector("user69@spamavert.com")); // false
-})();
